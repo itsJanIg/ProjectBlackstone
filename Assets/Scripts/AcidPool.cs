b1a652275e34c980a7f469dbb303c83e3b4243f9
ski901 <ski901wasmade2012@gmail.com>
2025-11-19 11:25:20 -0600

    Acid Pools Work - Health Works (no death yet)

--- Assets/Scripts/AcidPool.cs
+++ Assets/Scripts/AcidPool.cs
@@ -1,16 +1,75 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AcidPool : MonoBehaviour
+public class AcidDamage : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float damagePerTick = 5f;
+    public float damageInterval = 1f;
+    public float knockbackForce = 10f;
+
+    public Slider healthSlider;
+
+    private bool playerInside = false;
+    private Transform player;
+    private Rigidbody playerRb;
+
+    private void Start()
     {
-        
+        if (healthSlider == null)
+        {
+            healthSlider = FindFirstObjectByType<Slider>();
+            Debug.Log("Health slider auto-assigned: " + healthSlider);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log("TriggerEnter hit: " + other.name);
+
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player entered acid!");
+
+            playerInside = true;
+            player = other.transform;
+            playerRb = other.GetComponent<Rigidbody>();
+
+            StartCoroutine(DamageLoop());
+        }
+        else
+        {
+            Debug.Log("Object was not tagged Player");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInside = false;
+            Debug.Log("Player left acid.");
+        }
+    }
+
+    IEnumerator DamageLoop()
+    {
+        while (playerInside)
+        {
+            Debug.Log("Dealing damage tick!");
+
+            if (healthSlider != null)
+                healthSlider.value -= damagePerTick;
+            else
+                Debug.LogError("No health slider found!");
+
+            if (playerRb != null)
+            {
+                Vector3 direction = (player.position - transform.position).normalized;
+                playerRb.AddForce(direction * knockbackForce, ForceMode.Impulse);
+            }
+
+            yield return new WaitForSeconds(damageInterval);
+        }
     }
 }
